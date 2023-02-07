f102418c683234160ded1010e5009d014253d9d1
dmitriy-solovev <dmitriy.solovev@vkpay.com>
2023-02-07 15:07:54 +0300

    init project

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+   <SafeAreaView style={styles.container}>
+     <View>
+        <Text>Finance Tracking</Text>
+     </View>
+     <StatusBar style="auto"/>
+   </SafeAreaView>
+)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
